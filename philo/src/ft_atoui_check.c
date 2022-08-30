55419e7be2e728e60ba1aca2be1f7f399eb4d64a
Vhaefeli <vhaefeli@student.42lausanne.ch>
2022-08-30 11:20:28 +0200

    clean atoui

--- philo/src/ft_atoui_check.c
+++ philo/src/ft_atoui_check.c
@@ -16,15 +16,9 @@ unsigned int	ft_atoui_check(const char *str)
 		{
 			nb = (nb * 10) + (*str - 48);
 			power++;
-			printf("nb : %d, power: %d\n", nb, power);
 		}
 		else if (power == 9 && nb == 429496729 && *str >= '0' && *str <= '5')
-		{
 			nb = (nb * 10) + (*str - 48);
-			printf("int max %u\n", UINT_MAX);
-			printf("nb : %u, power: %d\n", nb, power);
-		}
-			
 		else if (power == 9 && nb < 429496729 && *str >= '0' && *str <= '9')
 			nb = (nb * 10) + (*str - 48);
 		else
@@ -35,11 +29,4 @@ unsigned int	ft_atoui_check(const char *str)
 		str++;
 	}
 	return (nb);
-}
-
-int main(int argc, char **argv)
-{
-	char *nb = argv[argc - 1];
-	printf("nb= %u\n", ft_atoui_check(nb));
-	return (0);
 }
